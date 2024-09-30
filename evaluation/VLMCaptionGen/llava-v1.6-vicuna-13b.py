05d3542daec12bc4681f948f28a0308f92d3138e
yutchina <155829679+yutchina@users.noreply.github.com>
2024-09-30 15:50:48 +0800

    Add files via upload
    Signed-off-by: yutchina <155829679+yutchina@users.noreply.github.com>

---
+++ evaluation/VLMCaptionGen/llava-v1.6-vicuna-13b.py
@@ -0,0 +1,31 @@
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import get_model_name_from_path
+from llava.eval.run_llava import eval_model
+
+model_path = "/data/codes/ty/CAPTURE/capture/ckpt/llava-v1.6-vicuna-13b"
+
+tokenizer, model, image_processor, context_len = load_pretrained_model(
+    model_path=model_path,
+    model_base=None,
+    model_name=get_model_name_from_path(model_path)
+)
+
+model_path = "/data/codes/ty/CAPTURE/capture/ckpt/llava-v1.6-vicuna-13b"
+prompt = "Please describe the image in the following format:  a (picture/photo/watercolor/sketch)  of <number> <color> <object> <appearance> in the style of <style>.  <They/It/He/She> <gesture> on the <background describe> in the <location> on a <weather> day, <action description>, <environment description>."
+image_file = "/data/codes/ty/evaluation/鸟人.png"
+
+args = type('Args', (), {
+    "model_path": model_path,
+    "model_base": None,
+    "model_name": get_model_name_from_path(model_path),
+    "query": prompt,
+    "conv_mode": None,
+    "image_file": image_file,
+    "sep": ",",
+    "temperature": 0,
+    "top_p": None,
+    "num_beams": 1,
+    "max_new_tokens": 512
+})()
+
+eval_model(args)
